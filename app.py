456aaa10227d81bf53c1f196ac38ebe65dde86d9
Leeeuijooo <euijoo3233@gmail.com>
2024-02-20 16:19:25 +0900

    Edit : app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '안녕하세요! Flask 앱입니다!'
+    return 'test1'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
